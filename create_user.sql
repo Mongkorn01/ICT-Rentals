922698399f480352d85da55d7b4c67a2409af8db
Mongkorn01 <mongkorn5340@gmail.com>
2026-04-28 13:17:39 +0700

    IF NOT EXISTS handle

--- create_user.sql
+++ create_user.sql
@@ -1,6 +1,6 @@
 -- Create User --
 USE `sec3_gr10_database`;
-CREATE USER 'itcs223'@'localhost' IDENTIFIED BY 'itCS223**';
+CREATE USER IF NOT EXISTS 'itcs223'@'localhost' IDENTIFIED BY 'itCS223**';
 GRANT ALL PRIVILEGES ON `sec3_gr10_database`.* TO 'itcs223'@'localhost';
 FLUSH PRIVILEGES;
 
